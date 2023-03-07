86be472c89204bd80a499ac8973ecc3c12031f7a
therandomspoon <107148755+therandomspoon@users.noreply.github.com>
2023-03-07 11:55:00 +0000

    Add files via upload

---
+++ download files/install-linux.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+URL="https://archive.org/download/randyt-main/randyt-main.zip"
+FILENAME="randyt-main.zip"
+curl -o $FILENAME $URL
---
+++ download files/install.bat
@@ -0,0 +1,4 @@
+@echo off
+echo downloading...
+curl -o randyt-main.zip https://archive.org/download/randyt-main/randyt-main.zip
+pause
